41f51928682006a09490436fc900bd9ea5361335
vellmoss <vellmoss@gmail.com>
2015-04-20 12:21:17 +0300

    неработающий набросок на обратную ссылку во 2-й форме

--- js/application.js
+++ js/application.js
@@ -44,7 +44,6 @@ $(function(){
             type: userTypes[u.get('type')]                              // извлекемых из экз. модели даннх u
         }));
         return false;                                                   // чтобы по submit не улетала на 404
-
     });
 
     $body.on('submit', '#form-2', function(){                           // собираю данные из 2-го активированного
@@ -73,14 +72,16 @@ $(function(){
         }));
         return false;                                                   // чтобы по submit не улетала на 404
     });
-
+/*          --------- попытка отработать стрелочку "назад <--" во второй модальной форме
     $body.on('click', '#form-2-a', function(){                          // возврат по ссылке на 1-й шаг         $('#modal').modal('hide');
         var $m = $('#modal');
-            $modal_content = $m.find('.modal-content');                 // дива контента шаблона там будет
-            $modal_content.html(templates.modal_1({step: 1}));              // вызов 1-го мод. элем. без всякого роута 1-й шаб.
+        var $t = {};
+        var $modal_content = $m.find('.modal-content');                 // дива контента шаблона там будет
+            $t.modal_1 = Handlebars.compile($("#modal-1-template").html());
+            $modal_content.html(templates.modal_1({step: 1}));          // вызов 1-го мод. элем. без всякого роута 1-й шаб.
             $m.modal('show');
     });
-
+*/
         $body.on('click', '.close-modal', function(){                   // по любой из ссылок 3-го мод. окна
         $('#modal').modal('hide');                                      // гашу его
     });
